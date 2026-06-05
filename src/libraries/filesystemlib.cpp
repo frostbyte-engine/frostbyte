5fc25043050092c744941693c62fdb0d8f5d4e49
TechHog <techhog@example.com>
2026-06-05 03:37:21 -0400

    dofile; loadfile's chunkname is now the relative path that was passed in

--- src/libraries/filesystemlib.cpp
+++ src/libraries/filesystemlib.cpp
@@ -168,6 +168,28 @@ static int fr_delfolder(lua_State* L) {
     return 0;
 }
 
+static int fr_dofile(lua_State* L) {
+    const char* relative_path = luaL_checkstring(L, 1);
+
+    checkPath(L, relative_path);
+
+    std::string path = FileSystem::workspace_path;
+    path.append(relative_path);
+    if (!std::filesystem::exists(path))
+        luaL_error(L, "failed to open file '%s'", relative_path);
+
+    std::ifstream file(path, std::ios::binary);
+    std::string content((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+
+    std::string bytecode = Luau::compile(content);
+    if (luau_load(L, relative_path, bytecode.data(), bytecode.size(), 0) != 0)
+        luaL_error(L, "%s", lua_tostring(L, -1));
+
+    int top_before = lua_gettop(L);
+    lua_call(L, 0, -1);
+
+    return lua_gettop(L) - top_before + 1;
+}
 static int fr_loadfile(lua_State* L) {
     const char* relative_path = luaL_checkstring(L, 1);
 
@@ -182,7 +204,7 @@ static int fr_loadfile(lua_State* L) {
     std::string content((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
 
     std::string bytecode = Luau::compile(content);
-    if (luau_load(L, "", bytecode.data(), bytecode.size(), 0) == 0)
+    if (luau_load(L, relative_path, bytecode.data(), bytecode.size(), 0) == 0)
         return 1;
 
     lua_pushnil(L);
@@ -199,6 +221,7 @@ void open_filesystemlib(lua_State* L) {
     env_expose(isfolder)
     env_expose(delfile)
     env_expose(delfolder)
+    env_expose(dofile)
     env_expose(loadfile)
 }
 
